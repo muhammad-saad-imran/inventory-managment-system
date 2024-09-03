50de768afc2e038bb0f7ce57786a76f09f2ae0b2
muhammad-saad-imran <muhammad.saad02@arbisoft.com>
2024-09-03 14:43:45 +0500

    feat: ui changes for order list

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -15,19 +15,19 @@ const OrderPage = () => {
       <SearchBar label="Orders" search={search} setSearch={setSearch} />
       <table className="w-full mt-3 bg-white">
         <tr>
-          <th className="py-5">ID</th>
-          <th className="py-5">Date</th>
-          <th className="py-5">Status</th>
-          <th className="py-5">Amount</th>
+          <th className="py-5 w-1/4">ID</th>
+          <th className="py-5 w-1/4">Date</th>
+          <th className="py-5 w-1/4">Status</th>
+          <th className="py-5 w-1/4">Amount</th>
         </tr>
         {filterProducts.map((item) => (
           <tr className="text-center hover:bg-black/[0.05] cursor-pointer">
-            <td className="py-5">{item.id}</td>
-            <td className="py-5">
+            <td className="py-5 w-1/4">{item.id}</td>
+            <td className="py-5 w-1/4">
               {formatDate({ date: item.order_date, outputDate: "MMM D, YYYY" })}
             </td>
-            <td className="py-5">{item.status}</td>
-            <td className="py-5">{formatPrice(item.total_amount)}</td>
+            <td className="py-5 w-1/4">{item.status}</td>
+            <td className="py-5 w-1/4">{formatPrice(item.total_amount)}</td>
           </tr>
         ))}
       </table>
